b76ea64b26f0adb73566349cffd796e50555f027
xspqwe <99954498+xspqwe@users.noreply.github.com>
2023-05-08 01:04:22 +0800

    Update tw.ts

--- app/locales/tw.ts
+++ app/locales/tw.ts
@@ -1,103 +1,104 @@
 import { SubmitKey } from "../store/config";
-import type { LocaleType } from "./index";
 
-const tw: LocaleType = {
-  WIP: "該功能仍在開發中……",
+const cn = {
+  WIP: "该功能仍在开发中……",
   Error: {
-    Unauthorized: "目前您的狀態是未授權，請前往設定頁面輸入授權碼。",
+    Unauthorized:
+      "现在是未授权状态，请点击左下角[设置](/#/settings)按钮输入访问密码使用密码到QQ群获取，点击左下角[小企鹅](http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=zu9GwYV_G4QoaAgLLHupQe6uhVkRHLS4&authKey=PdnVbJpRjMd7Cc%2BjIq2eoKxC3WcZ8fb8NcgAuD8R5hTmmQdgVzhQwKmMNF2OwjcL&noverify=0&group_code=594122721)加入QQ群。公益版完全免费，设置密码是为了防止直接共享的API被盗用，若需要独享账号可[点击发卡站购买](https://faka.51mfzy.top/)，在本平台使用包售后三个月，若自行登录使用不包售后",
   },
   ChatItem: {
-    ChatItemCount: (count: number) => `${count} 條對話`,
+    ChatItemCount: (count: number) => `${count} 条对话`,
   },
   Chat: {
-    SubTitle: (count: number) => `您已經與 ChatGPT 進行了 ${count} 條對話`,
+    SubTitle: (count: number) => `与 ChatGPT 的 ${count} 条对话`,
     Actions: {
-      ChatList: "查看訊息列表",
-      CompressedHistory: "查看壓縮後的歷史 Prompt",
-      Export: "匯出聊天紀錄",
-      Copy: "複製",
+      ChatList: "查看消息列表",
+      CompressedHistory: "查看压缩后的历史 Prompt",
+      Export: "导出聊天记录",
+      Copy: "复制",
       Stop: "停止",
-      Retry: "重試",
-      Delete: "刪除",
+      Retry: "重试",
+      Delete: "删除",
     },
-    Rename: "重命名對話",
-    Typing: "正在輸入…",
+    Rename: "重命名对话",
+    Typing: "正在输入…",
     Input: (submitKey: string) => {
-      var inputHints = `輸入訊息後，按下 ${submitKey} 鍵即可發送`;
+      var inputHints = `${submitKey} 发送`;
       if (submitKey === String(SubmitKey.Enter)) {
-        inputHints += "，Shift + Enter 鍵換行";
+        inputHints += "，Shift + Enter 换行";
       }
-      return inputHints;
+      return inputHints + "，/ 触发补全";
     },
-    Send: "發送",
+    Send: "发送",
     Config: {
       Reset: "重置默认",
       SaveAs: "另存为面具",
     },
   },
   Export: {
-    Title: "將聊天記錄匯出為 Markdown",
-    Copy: "複製全部",
-    Download: "下載檔案",
-    MessageFromYou: "來自您的訊息",
-    MessageFromChatGPT: "來自 ChatGPT 的訊息",
+    Title: "导出聊天记录为 Markdown",
+    Copy: "全部复制",
+    Download: "下载文件",
+    MessageFromYou: "来自你的消息",
+    MessageFromChatGPT: "来自 ChatGPT 的消息",
   },
   Memory: {
-    Title: "上下文記憶 Prompt",
-    EmptyContent: "尚未記憶",
-    Copy: "複製全部",
-    Send: "發送記憶",
-    Reset: "重設對話",
-    ResetConfirm: "重設後將清除目前對話記錄以及歷史記憶，確認重設？",
+    Title: "历史摘要",
+    EmptyContent: "对话内容过短，无需总结",
+    Send: "自动压缩聊天记录并作为上下文发送",
+    Copy: "复制摘要",
+    Reset: "重置对话",
+    ResetConfirm: "重置后将清空当前对话记录以及历史摘要，确认重置？",
   },
   Home: {
-    NewChat: "新的對話",
-    DeleteChat: "確定要刪除選取的對話嗎？",
-    DeleteToast: "已刪除對話",
-    Revert: "撤銷",
+    NewChat: "新的聊天",
+    DeleteChat: "确认删除选中的对话？",
+    DeleteToast: "已删除会话",
+    Revert: "撤销",
   },
   Settings: {
-    Title: "設定",
-    SubTitle: "設定選項",
+    Title: "设置",
+    SubTitle: "设置选项",
     Actions: {
-      ClearAll: "清除所有資料",
-      ResetAll: "重設所有設定",
-      Close: "關閉",
-      ConfirmResetAll: "您確定要重設所有設定嗎？",
-      ConfirmClearAll: "您確定要清除所有数据嗎？",
+      ClearAll: "清除所有数据",
+      ResetAll: "重置所有选项",
+      Close: "关闭",
+      ConfirmResetAll: "确认重置所有配置？",
+      ConfirmClearAll: "确认清除所有数据？",
     },
     Lang: {
       Name: "Language",
       All: "所有语言",
       Options: {
         cn: "简体中文",
-        en: "English",
-        tw: "繁體中文",
-        es: "Español",
-        it: "Italiano",
-        tr: "Türkçe",
-        jp: "日本語",
-        de: "Deutsch",
+        en: "简体中文",
+        tw: "简体中文",
+        es: "简体中文",
+        it: "简体中文",
+        tr: "简体中文",
+        jp: "简体中文",
+        de: "简体中文",
       },
     },
-    Avatar: "大頭貼",
+    Avatar: "头像",
     FontSize: {
-      Title: "字型大小",
-      SubTitle: "聊天內容的字型大小",
+      Title: "字体大小",
+      SubTitle: "聊天内容的字体大小",
     },
+
     Update: {
-      Version: (x: string) => `當前版本：${x}`,
+      Version: (x: string) => `当前版本：${x}`,
       IsLatest: "已是最新版本",
-      CheckUpdate: "檢查更新",
-      IsChecking: "正在檢查更新...",
-      FoundUpdate: (x: string) => `發現新版本：${x}`,
+      CheckUpdate: "检查更新",
+      IsChecking: "正在检查更新...",
+      FoundUpdate: (x: string) => `发现新版本：${x}`,
       GoToUpdate: "前往更新",
     },
-    SendKey: "發送鍵",
-    Theme: "主題",
-    TightBorder: "緊湊邊框",
+    SendKey: "发送键",
+    Theme: "主题",
+    TightBorder: "无边框模式",
     SendPreviewBubble: {
-      Title: "預覽氣泡",
+      Title: "预览气泡",
       SubTitle: "在预览气泡中预览 Markdown 内容",
     },
     Mask: {
@@ -106,86 +107,89 @@ const tw: LocaleType = {
     },
     Prompt: {
       Disable: {
-        Title: "停用提示詞自動補齊",
-        SubTitle: "在輸入框開頭輸入 / 即可觸發自動補齊",
+        Title: "禁用提示词自动补全",
+        SubTitle: "在输入框开头输入 / 即可触发自动补全",
       },
-      List: "自定義提示詞列表",
+      List: "自定义提示词列表",
       ListCount: (builtin: number, custom: number) =>
-        `內建 ${builtin} 條，用戶定義 ${custom} 條`,
-      Edit: "編輯",
+        `内置 ${builtin} 条，用户定义 ${custom} 条`,
+      Edit: "编辑",
       Modal: {
-        Title: "提示詞列表",
-        Add: "新增一條",
-        Search: "搜尋提示詞",
+        Title: "提示词列表",
+        Add: "新建",
+        Search: "搜索提示词",
       },
       EditModal: {
         Title: "编辑提示词",
       },
     },
     HistoryCount: {
-      Title: "附帶歷史訊息數",
-      SubTitle: "每次請求附帶的歷史訊息數",
+      Title: "附带历史消息数",
+      SubTitle: "每次请求携带的历史消息数",
     },
     CompressThreshold: {
-      Title: "歷史訊息長度壓縮閾值",
-      SubTitle: "當未壓縮的歷史訊息超過該值時，將進行壓縮",
+      Title: "历史消息长度压缩阈值",
+      SubTitle: "当未压缩的历史消息超过该值时，将进行压缩",
     },
     Token: {
       Title: "API Key",
-      SubTitle: "使用自己的 Key 可規避授權存取限制",
+      SubTitle: "使用自己的 Key 可绕过密码访问限制",
       Placeholder: "OpenAI API Key",
     },
+
     Usage: {
-      Title: "帳戶餘額",
+      Title: "余额查询",
       SubTitle(used: any, total: any) {
-        return `本月已使用 $${used}，訂閱總額 $${total}`;
+        return `本月已使用 $${used}，订阅总额 $${total}`;
       },
-      IsChecking: "正在檢查…",
-      Check: "重新檢查",
-      NoAccess: "輸入API Key查看餘額",
+      IsChecking: "正在检查…",
+      Check: "重新检查",
+      NoAccess: "输入 API Key 或访问密码查看余额",
     },
     AccessCode: {
-      Title: "授權碼",
-      SubTitle: "目前是未授權存取狀態",
-      Placeholder: "請輸入授權碼",
+      Title: "访问密码",
+      SubTitle: "已开启加密访问",
+      Placeholder: "请输入访问密码",
     },
     Model: "模型 (model)",
     Temperature: {
-      Title: "隨機性 (temperature)",
-      SubTitle: "值越大，回應越隨機",
+      Title: "随机性 (temperature)",
+      SubTitle: "值越大，回复越随机",
     },
     MaxTokens: {
-      Title: "單次回應限制 (max_tokens)",
-      SubTitle: "單次互動所用的最大 Token 數",
+      Title: "单次回复限制 (max_tokens)",
+      SubTitle: "单次交互所用的最大 Token 数",
     },
     PresencePenlty: {
-      Title: "話題新穎度 (presence_penalty)",
-      SubTitle: "值越大，越有可能擴展到新話題",
+      Title: "话题新鲜度 (presence_penalty)",
+      SubTitle: "值越大，越有可能扩展到新话题",
     },
   },
   Store: {
-    DefaultTopic: "新的對話",
-    BotHello: "請問需要我的協助嗎？",
-    Error: "出錯了，請稍後再嘗試",
+    DefaultTopic: "新的聊天",
+    BotHello: "若需要独享账号可[点击发卡站购买](https://faka.51mfzy.top/)，在本平台使用包售后三个月，若自行登录使用不包售后。有什么可以帮你的吗？继续使用输入你的问题或命令",
+    Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) =>
-        "這是 AI 與用戶的歷史聊天總結，作為前情提要：" + content,
+        "这是 ai 和用户的历史聊天总结作为前情提要：" + content,
       Topic:
-        "Use the language used by the user (e.g. en for english conversation, zh-hant for chinese conversation, etc.) to generate a title (at most 6 words) summarizing our conversation without any lead-in, quotation marks, preamble like 'Title:', direct text copies, single-word replies, quotation marks, translations, or brackets. Remove enclosing quotation marks. The title should make third-party grasp the essence of the conversation in first sight.",
+        "使用四到五个字直接返回这句话的简要主题，不要解释、不要标点、不要语气词、不要多余文本，如果没有主题，请直接返回“闲聊”",
       Summarize:
-        "Use the language used by the user (e.g. en-us for english conversation, zh-hant for chinese conversation, etc.) to summarise the conversation in at most 200 words. The summary will be used as prompt for you to continue the conversation in the future.",
+        "简要总结一下你和用户的对话，用作后续的上下文提示 prompt，控制在 200 字以内",
     },
   },
   Copy: {
-    Success: "已複製到剪貼簿中",
-    Failed: "複製失敗，請賦予剪貼簿權限",
+    Success: "已写入剪切板",
+    Failed: "复制失败，请赋予剪切板权限",
   },
   Context: {
-    Toast: (x: any) => `已設定 ${x} 條前置上下文`,
-    Edit: "前置上下文和歷史記憶",
-    Add: "新增一條",
+    Toast: (x: any) => `已设置 ${x} 条前置上下文`,
+    Edit: "当前对话设置",
+    Add: "新增预设对话",
   },
-  Plugin: { Name: "插件" },
+  Plugin: {
+    Name: "插件",
+  },
   Mask: {
     Name: "面具",
     Page: {
@@ -215,13 +219,14 @@ const tw: LocaleType = {
   },
   NewChat: {
     Return: "返回",
-    Skip: "跳过",
-    Title: "挑选一个面具",
-    SubTitle: "现在开始，与面具背后的灵魂思维碰撞",
-    More: "搜索更多",
+    Skip: "直接开始",
     NotShow: "不再展示",
     ConfirmNoShow: "确认禁用？禁用后可以随时在设置中重新启用。",
+    Title: "挑选一个面具",
+    SubTitle: "现在开始，与面具背后的灵魂思维碰撞",
+    More: "查看全部",
   },
+
   UI: {
     Confirm: "确认",
     Cancel: "取消",
@@ -231,4 +236,6 @@ const tw: LocaleType = {
   },
 };
 
-export default tw;
+export type LocaleType = typeof cn;
+
+export default cn;
