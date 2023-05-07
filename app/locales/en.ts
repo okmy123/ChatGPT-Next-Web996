a4452f8f744d2d674e4c4e9983803300fc8c43a0
xspqwe <99954498+xspqwe@users.noreply.github.com>
2023-05-08 01:02:47 +0800

    Update en.ts

--- app/locales/en.ts
+++ app/locales/en.ts
@@ -1,242 +1,241 @@
 import { SubmitKey } from "../store/config";
-import type { LocaleType } from "./index";
 
-const en: LocaleType = {
-  WIP: "Coming Soon...",
+const cn = {
+  WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized:
-      "Unauthorized access, please enter access code in settings page.",
+      "现在是未授权状态，请点击左下角[设置](/#/settings)按钮输入访问密码使用密码到QQ群获取，点击左下角[小企鹅](http://qm.qq.com/cgi-bin/qm/qr?_wv=1027&k=zu9GwYV_G4QoaAgLLHupQe6uhVkRHLS4&authKey=PdnVbJpRjMd7Cc%2BjIq2eoKxC3WcZ8fb8NcgAuD8R5hTmmQdgVzhQwKmMNF2OwjcL&noverify=0&group_code=594122721)加入QQ群。公益版完全免费，设置密码是为了防止直接共享的API被盗用，若需要独享账号可[点击发卡站购买](https://faka.51mfzy.top/)，在本平台使用包售后三个月，若自行登录使用不包售后",
   },
   ChatItem: {
-    ChatItemCount: (count: number) => `${count} messages`,
+    ChatItemCount: (count: number) => `${count} 条对话`,
   },
   Chat: {
-    SubTitle: (count: number) => `${count} messages with ChatGPT`,
+    SubTitle: (count: number) => `与 ChatGPT 的 ${count} 条对话`,
     Actions: {
-      ChatList: "Go To Chat List",
-      CompressedHistory: "Compressed History Memory Prompt",
-      Export: "Export All Messages as Markdown",
-      Copy: "Copy",
-      Stop: "Stop",
-      Retry: "Retry",
-      Delete: "Delete",
+      ChatList: "查看消息列表",
+      CompressedHistory: "查看压缩后的历史 Prompt",
+      Export: "导出聊天记录",
+      Copy: "复制",
+      Stop: "停止",
+      Retry: "重试",
+      Delete: "删除",
     },
-    Rename: "Rename Chat",
-    Typing: "Typing…",
+    Rename: "重命名对话",
+    Typing: "正在输入…",
     Input: (submitKey: string) => {
-      var inputHints = `${submitKey} to send`;
+      var inputHints = `${submitKey} 发送`;
       if (submitKey === String(SubmitKey.Enter)) {
-        inputHints += ", Shift + Enter to wrap";
+        inputHints += "，Shift + Enter 换行";
       }
-      return inputHints + ", / to search prompts";
+      return inputHints + "，/ 触发补全";
     },
-    Send: "Send",
+    Send: "发送",
     Config: {
-      Reset: "Reset to Default",
-      SaveAs: "Save as Mask",
+      Reset: "重置默认",
+      SaveAs: "另存为面具",
     },
   },
   Export: {
-    Title: "All Messages",
-    Copy: "Copy All",
-    Download: "Download",
-    MessageFromYou: "Message From You",
-    MessageFromChatGPT: "Message From ChatGPT",
+    Title: "导出聊天记录为 Markdown",
+    Copy: "全部复制",
+    Download: "下载文件",
+    MessageFromYou: "来自你的消息",
+    MessageFromChatGPT: "来自 ChatGPT 的消息",
   },
   Memory: {
-    Title: "Memory Prompt",
-    EmptyContent: "Nothing yet.",
-    Send: "Send Memory",
-    Copy: "Copy Memory",
-    Reset: "Reset Session",
-    ResetConfirm:
-      "Resetting will clear the current conversation history and historical memory. Are you sure you want to reset?",
+    Title: "历史摘要",
+    EmptyContent: "对话内容过短，无需总结",
+    Send: "自动压缩聊天记录并作为上下文发送",
+    Copy: "复制摘要",
+    Reset: "重置对话",
+    ResetConfirm: "重置后将清空当前对话记录以及历史摘要，确认重置？",
   },
   Home: {
-    NewChat: "New Chat",
-    DeleteChat: "Confirm to delete the selected conversation?",
-    DeleteToast: "Chat Deleted",
-    Revert: "Revert",
+    NewChat: "新的聊天",
+    DeleteChat: "确认删除选中的对话？",
+    DeleteToast: "已删除会话",
+    Revert: "撤销",
   },
   Settings: {
-    Title: "Settings",
-    SubTitle: "All Settings",
+    Title: "设置",
+    SubTitle: "设置选项",
     Actions: {
-      ClearAll: "Clear All Data",
-      ResetAll: "Reset All Settings",
-      Close: "Close",
-      ConfirmResetAll: "Are you sure you want to reset all configurations?",
-      ConfirmClearAll: "Are you sure you want to reset all data?",
+      ClearAll: "清除所有数据",
+      ResetAll: "重置所有选项",
+      Close: "关闭",
+      ConfirmResetAll: "确认重置所有配置？",
+      ConfirmClearAll: "确认清除所有数据？",
     },
     Lang: {
-      Name: "Language", // ATTENTION: if you wanna add a new translation, please do not translate this value, leave it as `Language`
-      All: "All Languages",
+      Name: "Language",
+      All: "所有语言",
       Options: {
         cn: "简体中文",
-        en: "English",
-        tw: "繁體中文",
-        es: "Español",
-        it: "Italiano",
-        tr: "Türkçe",
-        jp: "日本語",
-        de: "Deutsch",
+        en: "简体中文",
+        tw: "简体中文",
+        es: "简体中文",
+        it: "简体中文",
+        tr: "简体中文",
+        jp: "简体中文",
+        de: "简体中文",
       },
     },
-    Avatar: "Avatar",
+    Avatar: "头像",
     FontSize: {
-      Title: "Font Size",
-      SubTitle: "Adjust font size of chat content",
+      Title: "字体大小",
+      SubTitle: "聊天内容的字体大小",
     },
+
     Update: {
-      Version: (x: string) => `Version: ${x}`,
-      IsLatest: "Latest version",
-      CheckUpdate: "Check Update",
-      IsChecking: "Checking update...",
-      FoundUpdate: (x: string) => `Found new version: ${x}`,
-      GoToUpdate: "Update",
+      Version: (x: string) => `当前版本：${x}`,
+      IsLatest: "已是最新版本",
+      CheckUpdate: "检查更新",
+      IsChecking: "正在检查更新...",
+      FoundUpdate: (x: string) => `发现新版本：${x}`,
+      GoToUpdate: "前往更新",
     },
-    SendKey: "Send Key",
-    Theme: "Theme",
-    TightBorder: "Tight Border",
+    SendKey: "发送键",
+    Theme: "主题",
+    TightBorder: "无边框模式",
     SendPreviewBubble: {
-      Title: "Send Preview Bubble",
-      SubTitle: "Preview markdown in bubble",
+      Title: "预览气泡",
+      SubTitle: "在预览气泡中预览 Markdown 内容",
     },
     Mask: {
-      Title: "Mask Splash Screen",
-      SubTitle: "Show a mask splash screen before starting new chat",
+      Title: "面具启动页",
+      SubTitle: "新建聊天时，展示面具启动页",
     },
     Prompt: {
       Disable: {
-        Title: "Disable auto-completion",
-        SubTitle: "Input / to trigger auto-completion",
+        Title: "禁用提示词自动补全",
+        SubTitle: "在输入框开头输入 / 即可触发自动补全",
       },
-      List: "Prompt List",
+      List: "自定义提示词列表",
       ListCount: (builtin: number, custom: number) =>
-        `${builtin} built-in, ${custom} user-defined`,
-      Edit: "Edit",
+        `内置 ${builtin} 条，用户定义 ${custom} 条`,
+      Edit: "编辑",
       Modal: {
-        Title: "Prompt List",
-        Add: "Add One",
-        Search: "Search Prompts",
+        Title: "提示词列表",
+        Add: "新建",
+        Search: "搜索提示词",
       },
       EditModal: {
-        Title: "Edit Prompt",
+        Title: "编辑提示词",
       },
     },
     HistoryCount: {
-      Title: "Attached Messages Count",
-      SubTitle: "Number of sent messages attached per request",
+      Title: "附带历史消息数",
+      SubTitle: "每次请求携带的历史消息数",
     },
     CompressThreshold: {
-      Title: "History Compression Threshold",
-      SubTitle:
-        "Will compress if uncompressed messages length exceeds the value",
+      Title: "历史消息长度压缩阈值",
+      SubTitle: "当未压缩的历史消息超过该值时，将进行压缩",
     },
     Token: {
       Title: "API Key",
-      SubTitle: "Use your key to ignore access code limit",
+      SubTitle: "使用自己的 Key 可绕过密码访问限制",
       Placeholder: "OpenAI API Key",
     },
+
     Usage: {
-      Title: "Account Balance",
+      Title: "余额查询",
       SubTitle(used: any, total: any) {
-        return `Used this month $${used}, subscription $${total}`;
+        return `本月已使用 $${used}，订阅总额 $${total}`;
       },
-      IsChecking: "Checking...",
-      Check: "Check",
-      NoAccess: "Enter API Key to check balance",
+      IsChecking: "正在检查…",
+      Check: "重新检查",
+      NoAccess: "输入 API Key 或访问密码查看余额",
     },
     AccessCode: {
-      Title: "Access Code",
-      SubTitle: "Access control enabled",
-      Placeholder: "Need Access Code",
+      Title: "访问密码",
+      SubTitle: "已开启加密访问",
+      Placeholder: "请输入访问密码",
     },
-    Model: "Model",
+    Model: "模型 (model)",
     Temperature: {
-      Title: "Temperature",
-      SubTitle: "A larger value makes the more random output",
+      Title: "随机性 (temperature)",
+      SubTitle: "值越大，回复越随机",
     },
     MaxTokens: {
-      Title: "Max Tokens",
-      SubTitle: "Maximum length of input tokens and generated tokens",
+      Title: "单次回复限制 (max_tokens)",
+      SubTitle: "单次交互所用的最大 Token 数",
     },
     PresencePenlty: {
-      Title: "Presence Penalty",
-      SubTitle:
-        "A larger value increases the likelihood to talk about new topics",
+      Title: "话题新鲜度 (presence_penalty)",
+      SubTitle: "值越大，越有可能扩展到新话题",
     },
   },
   Store: {
-    DefaultTopic: "New Conversation",
-    BotHello: "Hello! How can I assist you today?",
-    Error: "Something went wrong, please try again later.",
+    DefaultTopic: "新的聊天",
+    BotHello: "若需要独享账号可[点击发卡站购买](https://faka.51mfzy.top/)，在本平台使用包售后三个月，若自行登录使用不包售后。有什么可以帮你的吗？继续使用输入你的问题或命令",
+    Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) =>
-        "This is a summary of the chat history between the AI and the user as a recap: " +
-        content,
+        "这是 ai 和用户的历史聊天总结作为前情提要：" + content,
       Topic:
-        "Please generate a four to five word title summarizing our conversation without any lead-in, punctuation, quotation marks, periods, symbols, or additional text. Remove enclosing quotation marks.",
+        "使用四到五个字直接返回这句话的简要主题，不要解释、不要标点、不要语气词、不要多余文本，如果没有主题，请直接返回“闲聊”",
       Summarize:
-        "Summarize our discussion briefly in 200 words or less to use as a prompt for future context.",
+        "简要总结一下你和用户的对话，用作后续的上下文提示 prompt，控制在 200 字以内",
     },
   },
   Copy: {
-    Success: "Copied to clipboard",
-    Failed: "Copy failed, please grant permission to access clipboard",
+    Success: "已写入剪切板",
+    Failed: "复制失败，请赋予剪切板权限",
   },
   Context: {
-    Toast: (x: any) => `With ${x} contextual prompts`,
-    Edit: "Contextual and Memory Prompts",
-    Add: "Add a Prompt",
+    Toast: (x: any) => `已设置 ${x} 条前置上下文`,
+    Edit: "当前对话设置",
+    Add: "新增预设对话",
   },
   Plugin: {
-    Name: "Plugin",
+    Name: "插件",
   },
   Mask: {
-    Name: "Mask",
+    Name: "面具",
     Page: {
-      Title: "Prompt Template",
-      SubTitle: (count: number) => `${count} prompt templates`,
-      Search: "Search Templates",
-      Create: "Create",
+      Title: "预设角色面具",
+      SubTitle: (count: number) => `${count} 个预设角色定义`,
+      Search: "搜索角色面具",
+      Create: "新建",
     },
     Item: {
-      Info: (count: number) => `${count} prompts`,
-      Chat: "Chat",
-      View: "View",
-      Edit: "Edit",
-      Delete: "Delete",
-      DeleteConfirm: "Confirm to delete?",
+      Info: (count: number) => `包含 ${count} 条预设对话`,
+      Chat: "对话",
+      View: "查看",
+      Edit: "编辑",
+      Delete: "删除",
+      DeleteConfirm: "确认删除？",
     },
     EditModal: {
       Title: (readonly: boolean) =>
-        `Edit Prompt Template ${readonly ? "(readonly)" : ""}`,
-      Download: "Download",
-      Clone: "Clone",
+        `编辑预设面具 ${readonly ? "（只读）" : ""}`,
+      Download: "下载预设",
+      Clone: "克隆预设",
     },
     Config: {
-      Avatar: "Bot Avatar",
-      Name: "Bot Name",
+      Avatar: "角色头像",
+      Name: "角色名称",
     },
   },
   NewChat: {
-    Return: "Return",
-    Skip: "Skip",
-    Title: "Pick a Mask",
-    SubTitle: "Chat with the Soul behind the Mask",
-    More: "Find More",
-    NotShow: "Not Show Again",
-    ConfirmNoShow: "Confirm to disable？You can enable it in settings later.",
+    Return: "返回",
+    Skip: "直接开始",
+    NotShow: "不再展示",
+    ConfirmNoShow: "确认禁用？禁用后可以随时在设置中重新启用。",
+    Title: "挑选一个面具",
+    SubTitle: "现在开始，与面具背后的灵魂思维碰撞",
+    More: "查看全部",
   },
 
   UI: {
-    Confirm: "Confirm",
-    Cancel: "Cancel",
-    Close: "Close",
-    Create: "Create",
-    Edit: "Edit",
+    Confirm: "确认",
+    Cancel: "取消",
+    Close: "关闭",
+    Create: "新建",
+    Edit: "编辑",
   },
 };
 
-export default en;
+export type LocaleType = typeof cn;
+
+export default cn;
