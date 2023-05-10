0e4c17b5ac6981e4781794517faeee7cca207729
xspqwe <99954498+xspqwe@users.noreply.github.com>
2023-05-10 09:53:27 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -117,7 +117,7 @@ export function SideBar(props: { className?: string }) {
           icon={<PluginIcon />}
           text={shouldNarrow ? undefined : Locale.Plugin.Name}
           className={styles["sidebar-bar-button"]}
-          onClick={() => window.open"https://faka.51mfzy.top/"}
+          onClick={() => window.location.href="https://faka.51mfzy.top/"}
           shadow
         />
       </div>
