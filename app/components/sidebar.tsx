07f07619a3f6272929c6d1f0a4ab71d04dc9b8a2
okmy123 <136219898+okmy123@users.noreply.github.com>
2023-10-09 11:38:25 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -98,7 +98,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>ChatGPT官方API接口版</div>
         <div className={styles["sidebar-sub-title"]}>
-          进QQ交流群594122721免费获取访问密码
+          进QQ交流群594122721售后无忧
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
